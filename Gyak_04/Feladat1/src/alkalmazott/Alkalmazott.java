f61d45a960b840d169d5490a6f473b6306b790e3
NGergo-AEMOUD <78854615+NGergo-AEMOUD@users.noreply.github.com>
2021-03-08 09:45:00 +0100

    Osztály befejezve(get,set,const)

--- Gyak_04/Feladat1/src/alkalmazott/Alkalmazott.java
+++ Gyak_04/Feladat1/src/alkalmazott/Alkalmazott.java
@@ -1,10 +1,29 @@
 package alkalmazott;
 
 public class Alkalmazott {
-	String nev;
-	int fizetes;
+	private final String nev;
+	private int fizetes;
 	
-	void Fizetesnovel(double szazalek) {
+	public Alkalmazott(String nev, int fizetes) {
+		super();
+		this.nev = nev;
+		this.fizetes = fizetes;
+	}
+
+	public String getNev() {
+		return nev;
+	}
+
+
+	public int getFizetes() {
+		return fizetes;
+	}
+
+	public void setFizetes(int fizetes) {
+		this.fizetes = fizetes;
+	}
+
+	public void Fizetesnovel(double szazalek) {
 		fizetes+=(int)(fizetes*(szazalek/100));
 	}
 
--- Gyak_04/Feladat1/src/alkalmazott/AlkalmazottApp.java
+++ Gyak_04/Feladat1/src/alkalmazott/AlkalmazottApp.java
@@ -3,9 +3,11 @@ package alkalmazott;
 public class AlkalmazottApp {
 
 	public static void main(String[] args) {
-		Alkalmazott a1 = new Alkalmazott();
-		a1.nev="Nagy Gergő";
-		a1.fizetes=1000000;
+		Alkalmazott a1 = new Alkalmazott("Nagy Gergő",1000000);
+		/*a1.nev="Nagy Gergő";
+		a1.fizetes=1000000;*/
+		System.out.println(a1);
+		a1.Fizetesnovel(35);
 		System.out.println(a1);
 	}
 
